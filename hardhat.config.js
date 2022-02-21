b0de04c20155c2e01ccafca60eaabd49706e64a7
jean202 <hb13pud731@gmail.com>
2022-02-21 20:10:01 +0900

    fitst commit youtuobe 22:25

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
+const projectId = "abd91034b96340a8a8a2920493229893"
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {}
+  },
+  solidity: "0.8.4",
+};
