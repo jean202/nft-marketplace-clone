c40f13ef3f6988a17822a3d43ed5a6d46116eb6e
jean202 <hb13pud731@gmail.com>
2022-02-22 19:40:58 +0900

    46:33 commit

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -9,13 +9,16 @@ import "hardhat/console.sol";
 
 contract NFT is ERC721URIStorage {
     using Counters for Counters.Counter;
+    // 1씩 증가하는 tokenId, 토큰 식별자
     Counters.Counter private _tokenIds;
+    // 마켓플레이스의 주소 : nft와 상호작용 하는
     address contractAddress;
 
     constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
         contractAddress = marketplaceAddress;
     }
 
+    // 새로운 토큰을 발행한다
     function createToken(string memory tokenURI) public returns (uint) {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
